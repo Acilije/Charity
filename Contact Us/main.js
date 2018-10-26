68f004d34bc02f4be995738c491878cd5711ae0b
BIT student <classroom@bgit.rs>
2018-10-26 20:55:51 +0200

    finished

--- Contact Us/main.js
+++ Contact Us/main.js
@@ -2,59 +2,58 @@ $(function () {
     var name = $('.name');
     var email = $('.email');
     var subject = $('.subject');
-    var nameInput = $('.name').val();
-    var emailInput = $('.email').val();
-    var subjectInput = $('.subject').val();
     var submit = $('#submit');
     var warning = $('.warning-note');
-    
 
-    // submit.on('click', function () {
-    //     if (nameInput == "" || emailInput == "" || subjectInput == "") {
-    //         warning.show()
-    //         // warning.css('display','block');            
-    //     };
-    // })
-
-    
-    // submit.on('click', function () {
-    //     if (nameInput == "") {
-    //         name.css('border' , '2px solid red');          
-    //     };
-    // })
-
-    // submit.on('click', function () {
-    //     if (emailInput == "") {
-    //         email.css('border' , '2px solid red');          
-    //     };
-    // })
-
-    // submit.on('click', function () {
-    //     if (subjectInput == "") {
-    //         subject.css('border' , '2px solid red');          
-    //     };
-    // })    
 
     submit.on('click', function () {
-        if (nameInput === "") {
-            warning.show()
-            name.css('border' , '2px solid red');          
+        var nameInput = name.val()
+        var emailInput = email.val()
+        var subjectInput = subject.val();
+
+        if (nameInput == "") {
+            warning.show();
+            name.css('border', '2px solid #b62d32');
+        }
+        else {
+            name.css('border', '2px solid #e4e2de');
+        }
+
+        if (emailInput == "") {
+            warning.show();
+            email.css('border', '2px solid #b62d32');
+        }
+        else {
+            email.css('border', '2px solid #e4e2de');
+        }
+
+        if (subjectInput == "") {
+            warning.show();
+            subject.css('border', '2px solid #b62d32');
+        }
+        else {
+            subject.css('border', '2px solid #e4e2de');
+        }
+
+        if (nameInput !== "" && emailInput !== "" && subjectInput !== "") {
+            warning.hide();
+
+            // nece input submit pa je div. ovo je post, samo ubaci adresu
+           $.post("/", {
+                name: nameInput,
+                email: emailInput,
+                subject: subjectInput,
+            }, function (data) {
+
+            })
+
+
+            //  Reset value
+            name.val('');
+            email.val('');
+            subject.val('');
+
         }
     })
-
-    submit.on('click', function () {
-        if (emailInput === "") {
-            warning.show()
-            email.css('border' , '2px solid red');          
-        }
-    })
-
-    submit.on('click', function () {
-        if (subjectInput === "") {
-            warning.show()
-            subject.css('border' , '2px solid red');          
-        }
-    })
-
 
 })
